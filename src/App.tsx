4e2f05a742d84a2969ad7775e0fb1f8ac5057a52
Vsanotskyi <vsanotskyi@gmail.com>
2024-03-26 11:51:22 +0200

    c1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import {Suspense, lazy} from 'react';
-import {Navigate, Route, Routes} from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
-import {Loading} from './components';
+import { Loading } from './components';
 
-const Layout = lazy(() => import('./layout/MainLayout'));
+const MainLayout = lazy(() => import('./layout/MainLayout'));
 const MoviePages = lazy(() => import('./pages/MoviePages'));
 const GenrePages = lazy(() => import('./pages/GenrePages'));
 const SearchPage = lazy(() => import('./pages/SearchPage'));
@@ -11,36 +11,36 @@ const DetailsPages = lazy(() => import('./pages/DetailsPages'));
 const ErrorPages = lazy(() => import('./pages/ErrorPages'));
 
 const App = () => {
-    return (
-        <Suspense fallback={<Loading/>}>
-            <Routes>
-                <Route path={'/'}
-                       element={<Navigate to={'/movies'}/>}
-                />
-                <Route
-                    path="/movies"
-                    element={<Layout/>}
-                >
-                    <Route
-                        index
-                        element={<MoviePages/>}
-                    />
-                    <Route path={'genre/:name/:id'}
-                           element={<GenrePages/>}
-                    />
-                    <Route path={'search/:searchName'}
-                           element={<SearchPage/>}
-                    />
-                    <Route path={'details/:id'}
-                           element={<DetailsPages/>}
-                    />
-                </Route>
-                <Route path={'*'}
-                       element={<ErrorPages/>}
-                />
-            </Routes>
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path={'/'}
+               element={<Navigate to={'/movies'} />}
+        />
+        <Route
+          path="/movies"
+          element={<MainLayout />}
+        >
+          <Route
+            index
+            element={<MoviePages />}
+          />
+          <Route path={'genre/:name/:id'}
+                 element={<GenrePages />}
+          />
+          <Route path={'search/:searchName'}
+                 element={<SearchPage />}
+          />
+          <Route path={'details/:id'}
+                 element={<DetailsPages />}
+          />
+        </Route>
+        <Route path={'*'}
+               element={<ErrorPages />}
+        />
+      </Routes>
+    </Suspense>
+  );
 };
 
 export default App;
