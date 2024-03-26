4af6e90628b1b7843ff976d840dd47d37c87ae5a
Vsanotskyi <vsanotskyi@gmail.com>
2024-03-26 13:32:07 +0200

    c7

--- src/components/Movie/MovieItem.tsx
+++ src/components/Movie/MovieItem.tsx
@@ -2,71 +2,76 @@ import {FC} from 'react';
 import {useNavigate} from 'react-router-dom';
 
 import {
-    Card,
-    CardContent,
-    CardMedia,
-    Typography,
-    Rating,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+  Rating,
 } from '@mui/material';
 
 import {IMovie} from '../../interfaces';
 import defaultImage from '../../defaultImage/default-image.jpg';
 import {defaultUrlImage} from '../../constants';
+import {useAppDispatch, useSearchToggle, useTheme} from '../../hooks';
+import {toggleShowSearch} from '../../store/search';
 import css from './MovieItem.module.css';
-import {useTheme} from '../../hooks';
 
 interface IProps {
-    item: IMovie;
+  item: IMovie;
 }
 
 const MovieItem: FC<IProps> = ({item}) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
 
-    const {title, poster_path, vote_average, id} = item;
+  const {title, poster_path, vote_average, id} = item;
 
-    const urlImg = defaultUrlImage + poster_path;
+  const urlImg = defaultUrlImage + poster_path;
 
-    const checkUrl = urlImg.split('/').slice(-1).toString() === 'null';
+  const checkUrl = urlImg.split('/').slice(-1).toString() === 'null';
 
-    const handleClick = () => {
-        navigate(`/movies/details/${id}`);
-    };
+  const showSearch = useSearchToggle().searchToggle;
 
-    const theme = useTheme().theme;
-    const currentTheme = theme ? 'dark-theme' : 'light-theme';
+  const handleClick = () => {
+    showSearch && dispatch(toggleShowSearch());
+    navigate(`/movies/details/${id}`);
+  };
 
-    return (
-        <li>
-            <Card sx={{width: 300, height: 400}}
-                  onClick={handleClick}
-            >
-                <CardMedia
-                    component="img"
-                    alt={title}
-                    height="300"
-                    image={!checkUrl ? urlImg : defaultImage}
-                />
-                <CardContent className={css[currentTheme]}>
-                    <Typography gutterBottom
-                                variant="h6"
-                                component="div"
-                    >
-                        <p className={css.overflowWrap}>{title}</p>
-                    </Typography>
-                    <Typography variant="body2"
-                                color="text.secondary"
-                    >
-                        <Rating name="read-only"
-                                value={vote_average / 2}
-                                readOnly
-                                precision={0.5}
-                                size="small"
-                        />
-                    </Typography>
-                </CardContent>
-            </Card>
-        </li>
-    );
+  const theme = useTheme().theme;
+  const currentTheme = theme ? 'dark-theme' : 'light-theme';
+
+  return (
+    <li>
+      <Card sx={{width: 300, height: 400}}
+            onClick={handleClick}
+      >
+        <CardMedia
+          component="img"
+          alt={title}
+          height="300"
+          image={!checkUrl ? urlImg : defaultImage}
+        />
+        <CardContent className={css[currentTheme]}>
+          <Typography gutterBottom
+                      variant="h6"
+                      component="div"
+          >
+            <p className={css.overflowWrap}>{title}</p>
+          </Typography>
+          <Typography variant="body2"
+                      color="text.secondary"
+          >
+            <Rating name="read-only"
+                    value={vote_average / 2}
+                    readOnly
+                    precision={0.5}
+                    size="small"
+            />
+          </Typography>
+        </CardContent>
+      </Card>
+    </li>
+  );
 };
 
 export {MovieItem};
